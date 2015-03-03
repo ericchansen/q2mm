cb8051ec96d34d8101d31bed1850ebc350a69273
Eric Carl Hansen <ericchansen@gmail.com>
2015-03-03 17:58:12 -0500

    Modified how arguments are passed.
    Improved the handling of argument parsers. This helped make
    parameters.py less redundant.

--- optimizer.py
+++ optimizer.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python
 '''
-Contains general code related to all optimization techniques.
-
-Instead of keeping parameters in a list, we could store them based
-upon their MM3* index (row, column). Then we wouldn't need to save
-entire parameter sets, significantly reducing the memory usage.
+General code related to all optimization techniques.
 '''
 import argparse
 import copy
 import logging
 import os
+import sys
 
 from calculate import run_calculate
 from compare import calc_x2, import_steps
 from datatypes import FF, MM3, UnallowedNegative
+from parameters import return_parameters_parser, select_parameters
 
 logger = logging.getLogger(__name__)
 
 class Optimizer(object):
+    '''
+    All specific optimization classes (ex. Gradient, Simplex, etc.)
+    inherit from this class. This class contains methods that should
+    be general to all optimization techniques.
+    '''
     def __init__(self):
         self.com_ref = None
         self.com_cal = None
         self.data_ref = None
         self.init_ff = None
 
+    def return_optimizer_parser(self, add_help=False, parents=[]):
+        '''
+        Return an argparse.ArgumentParser object that is general to
+        all optimization techniques.
+        '''
+        parameters_parser = return_parameters_parser(add_help=False)
+        parents.append(parameters_parser)
+        parser = argparse.ArgumentParser(add_help=add_help, parents=parents)
+        
+        parser.add_argument(
+            '--calculate', '-c', type=str, metavar='" commands for calculate.py"',
+            help=('These commands produce the calculated data. Leave one space '
+                  'after the first quotation mark enclosing the arguments.'))
+        parser.add_argument(
+            '--directory', '-d', type=str, metavar='directory', default=os.getcwd(),
+            help=('Directory where data and force field files are located. '
+                  'Calculations will be performed in this directory.'))
+        parser.add_argument(
+            '--reference', '-r', type=str, metavar='" commands for calculate.py"',
+            help=('These commands produce the reference data. Leave one space '
+                  'after the first quotation mark enclosing the arguments.'))
+        return parser
+
     def central_diff_derivs(self, init_ff, central_ffs):
         for i in xrange(0, len(central_ffs), 2):
             init_ff.params[i / 2].der1 = (central_ffs[i].x2 - central_ffs[i + 1].x2) * 0.5 # 18
-            init_ff.params[i / 2].der2 = central_ffs[i].x2 + central_ffs[i + 1].x2 - 2 * init_ff.x2 # 19
+            init_ff.params[i / 2].der2 = central_ffs[i].x2 + \
+                central_ffs[i + 1].x2 - 2 * init_ff.x2 # 19
         logger.info('1st derivatives: {}'.format([x.der1 for x in init_ff.params]))
         logger.info('2nd derivatives: {}'.format([x.der2 for x in init_ff.params]))
 
@@ -42,13 +69,7 @@ class Optimizer(object):
 
     def params_diff(self, params, mode='central'):
         '''
-        Perform forward or central differentiation of parameters.
-
-        Need means of adjusting the parameter step size. When checking the parameter,
-        perhaps it should account for the adjusted step size.
-
-        I would say this function is rather memory insensitive and could
-        use some improvements.
+        Perform forward or central differentiation on parameters.
         '''
         logger.info('{} differentiation - {} parameters'.format(mode, len(params)))
         import_steps(params)
@@ -59,12 +80,6 @@ class Optimizer(object):
                     ff_forward = FF()
                     ff_forward.params = copy.deepcopy(params)
                     ff_forward.method = 'forward {} {}'.format(param.mm3_row, param.mm3_col)
-                    # commented region now done in compare.import_steps
-                    # if isinstance(ff_forward.params[i].step, basestring):
-                    #     ff_forward.params[i].value += ff_forward.params[i].value * \
-                    #         float(ff_forward.params[i].step)
-                    # else:
-                    #     ff_forward.params[i].value += ff_forward.params[i].step
                     ff_forward.params[i].value += ff_forward.params[i].step
                     ff_forward.params[i].check_value()
                     if mode == 'central':
@@ -88,59 +103,50 @@ class Optimizer(object):
         logger.info('generated {} force fields for {} differentiation'.format(len(diff_ffs), mode))
         return diff_ffs
 
-    def return_optimizer_parser(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--calculate', '-c', type=str,
-                            metavar='" commands for calculate.py"',
-                            help=('These commands produce the calculated data. Leave one space '
-                                  'after the 1st quotation mark enclosing the arguments.'))
-        parser.add_argument('--directory', '-d', type=str, metavar='directory', default=os.getcwd(),
-                            help='Directory where data and force field files are located.')
-        parser.add_argument('--ptypes', '-pt', type=str, nargs='+')
-        parser.add_argument('--pfile', '-pf', type=str)
-        parser.add_argument('--reference', '-r', type=str,
-                            metavar='" commands for calculate.py"',
-                            help=('These commands produce the reference data. Leave one space '
-                                  'after the 1st quotation mark enclosing the arguments.'))
-        return parser
-
     def setup(self, args):
-        opts = self.parse(args)
+        '''
+        General setup is applied to the start of any optimization technique.
+        '''
+        if self.__class__.__name__ == 'Gradient':
+            gradient_parser = self.return_gradient_parser(add_help=True)
+            parser = self.return_optimizer_parser(
+                add_help=False, parents=[gradient_parser])
+        elif self.__class__.__name__ == 'Simplex':
+            simplex_parser = self.return_simplex_parser(add_help=True)
+            parser = self.return_optimizer_parser(
+                add_help=False, parents=[simplex_parser])
+
+        opts = parser.parse_args(args)
+
+        if self.__class__.__name__ == 'Gradient':
+            self.setup_gradient(opts)
+        elif self.__class__.__name__ == 'Simplex':
+            self.setup_simplex(opts)
+
         logger.info('--- setup {} ---'.format(type(self).__name__))
+
+        # not needed with parameter groups?
         self.com_ref = opts.reference
         self.com_cal = opts.calculate
+
+        # load existing force field
         self.init_ff = MM3(os.path.join(opts.directory, 'mm3.fld'))
         self.init_ff.import_ff()
         self.init_ff.method = 'initial'
         logger.info('{} ff loaded from {} - {} parameters'.format(
                 self.init_ff.method, self.init_ff.path, len(self.init_ff.params)))
-
-        params_to_optimize = []
-        if opts.ptypes:
-            params_to_optimize.extend([x for x in self.init_ff.params if x.ptype in opts.ptypes])
-        if opts.pfile:
-            with open(os.path.join(opts.directory, opts.pfile), 'r') as f:
-                for line in f:
-                    line = line.partition('#')[0]
-                    cols = line.split()
-                    if cols:
-                        mm3_row, mm3_col = int(cols[0]), int(cols[1])
-                        allow_negative = None
-                        for arg in cols[2:]:
-                            if any(arg == x for x in ('allowneg', 'neg', 'negative')):
-                                allow_negative = True
-                                logger.log(7, 'param {} {} allowed negative values'.format(
-                                        mm3_row, mm3_col))
-                        for param in self.init_ff.params:
-                            if mm3_row == param.mm3_row and mm3_col == param.mm3_col:
-                                param._allow_negative = allow_negative
-                                params_to_optimize.append(param)
+        
+        # select parameters from existing force field
+        # reduce number of initial parameters to only those selected
+        params_to_optimize = select_parameters(opts, ff=self.init_ff)
         for param in params_to_optimize:
             param.check_value()
         # import_steps(params_to_optimize)
         self.init_ff.params = params_to_optimize
         logger.info('{} parameters selected for optimization'.format(len(self.init_ff.params)))
 
+        # evaluate the reference data
+        # not needed with parameter groups?
         self.data_ref = run_calculate(self.com_ref.split())
         
         return opts
