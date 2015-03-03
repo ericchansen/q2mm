6a0b0cc2bc5846ea197dfde885dbb9423ac51a24
Eric Carl Hansen <ericchansen@gmail.com>
2015-03-03 12:34:56 -0500

    Improved descriptions. Added support for parameter files.

--- parameters.py
+++ parameters.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 '''
-Short script that prints the location of parameters in a force field
-file.
+Selects parameters from force fields.
 '''
 import argparse
+from argparse import RawTextHelpFormatter
 import logging
 import sys
 
@@ -12,11 +12,38 @@ from datatypes import MM3
 logger = logging.getLogger(__name__)
 
 def parse_parameters(args):
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--all', '-a', action='store_true', help='Select all parameters.')
-    parser.add_argument('--ffpath', '-f', metavar='mm3.fld', default='mm3.fld', help='Path to force field.')
-    parser.add_argument('--noprint', '-n', action='store_true', help="Don't print parameter locations.")
-    parser.add_argument('--ptypes', '-p', nargs='+', default=[], help='Select these parameter types.')
+    '''
+    Parses command line arguments. Grabs the selected parameters.
+    '''
+    parser = argparse.ArgumentParser(
+        formatter_class=RawTextHelpFormatter,
+        description=(__doc__ + 
+                     '''
+PTYPES:
+ae   - equilibrium angles
+af   - angle force constants
+be   - equilibrium bond lengths
+bf   - bond force constants
+df   - dihedral force constants
+imp1 - improper torsions (1st MM3* column)
+imp2 - improper torsions (2nd MM3* column)
+sb   - stretch-bend force constants
+q    - bond dipoles'''))
+    parser.add_argument(
+        '--all', '-a', action='store_true',
+        help='Select all available parameters from the force field.')
+    parser.add_argument(
+        '--ffpath', '-f', metavar='mm3.fld', default='mm3.fld',
+        help='Path to force field.')
+    parser.add_argument(
+        '--doprint', '-dp', action='store_true',
+        help='Print locations in force field of the selected parameters.')
+    parser.add_argument(
+        '--pfile', '-pf', type=str, metavar='filename',
+        help='Use a file to select parameters. Allows advanced options.')
+    parser.add_argument(
+        '--ptypes', '-pt', nargs='+', default=[],
+        help='Select these parameter types.')
     opts = parser.parse_args(args)
 
     if opts.all:
@@ -26,17 +53,47 @@ def parse_parameters(args):
     ff.import_ff()
     logger.info('{} params in {}'.format(len(ff.params), ff.path))
 
-    params = []
-    for param in ff.params:
-        if param.ptype in opts.ptypes:
-            params.append(param)
-    logger.info('selected {} params'.format(len(params)))            
+    selected_params = []
+    if opts.ptypes:
+        selected_params.extend([x for x in ff.params if x.ptype in opts.ptypes])
 
-    if not opts.noprint:
-        for param in params:
-            print('{} {}'.format(param.mm3_row, param.mm3_col))
-    
-    return params
+    if opts.pfile:
+        temp_params = []
+        with open(opts.pfile, 'r') as f:
+            for line in f:
+                line = line.partition('#')[0]
+                cols = line.split()
+                if cols:
+                    mm3_row, mm3_col = int(cols[0]), int(cols[1])
+                    allow_negative = None
+                    group_label = None
+                    for arg in cols[2:]:
+                        # if any(arg == x for x in ('allowneg', 'neg', 'negative')):
+                        if 'neg' in arg:
+                            allow_negative = True
+                            # logger.log(7, 'param {} {} allowed negative values'.format(
+                            #         mm3_row, mm3_col))
+                        if arg.startswith('g'):
+                            group_label = arg[1:]
+                    temp_params.append((mm3_row, mm3_col, allow_negative, group_label))
+        for param in ff.params:
+            for temp_param in temp_params:
+                if param.mm3_row == temp_param[0] and param.mm3_col == temp_param[1]:
+                    param._allow_negative = temp_param[2]
+                    param.group = temp_param[3]
+                    selected_params.append(param)
+                                       
+    if opts.doprint:
+        for param in selected_params:
+            # print('{} {}'.format(param.mm3_row, param.mm3_col))
+            try:
+                print('{}[{},{}]({})({})({})'.format(
+                        param.ptype, param.mm3_row, param.mm3_col, param.value,
+                        param.group, param.allow_negative))
+            except:
+                print param
+            
+    return selected_params
 
 if __name__ == '__main__':
     import logging.config
