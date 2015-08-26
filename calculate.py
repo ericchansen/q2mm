37e11dccf26af9a5cd8d6ad3cfb7bb42d0fcb99a
Eric Carl Hansen <ericchansen@gmail.com>
2015-08-26 13:23:42 -0400

    Modified calculate logging.

--- calculate.py
+++ calculate.py
@@ -334,13 +334,13 @@ class Mae(object):
             com += COM_FORM.format('MINI', 1, 0, 2500, 0, 0.05, 0, 0, 0)
         with open(os.path.join(self.directory, self.name_com), 'w') as f:
             f.write(com)
-        logger.log(0, 'WROTE: {}'.format(os.path.join(self.directory, self.name_com)))
+        logger.log(5, 'WROTE: {}'.format(os.path.join(self.directory, self.name_com)))
 
 def collect_data(commands, inps, ff_dir, sub_names=None):
     if any([x in COM_LOAD_FF for x in commands]):
         coms_need_ff = [x for x in commands if x in COM_LOAD_FF]
         if sub_names is None:
-            logger.log(20, '  -- Must read FF for {}.'.format(
+            logger.log(10, '  -- Must read FF for {}.'.format(
                     ' '.join(coms_need_ff)))
             ff = datatypes.import_ff(os.path.join(ff_dir, 'mm3.fld'))
     outs = {}
