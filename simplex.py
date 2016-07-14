ericchansen/q2mm

commit e40261269dbc07416b87b8a3b14ec2fd5b014f97

Variable was being recalculated inside loop
A float that didn't change was being recalculated over and over
inside a loop. Changed such that it is only calculcated once before
the loop.

Also changed error handling to accomodate this.

Changes made based upon Per-Ola's suggestion.

Reference issue #22.

simplex.py
@@ -201,43 +201,26 @@ class Simplex(opt.Optimizer):
             ref_ff.method = 'REFLECTION'
             ref_ff.params = copy.deepcopy(best_ff.params)
             # Need score difference sum for weighted inversion.
-            # If zero, should break.
-            score_diff_sum = sum([x.score - self.new_ffs[-1].score
-                                  for x in self.new_ffs[:-1]])
-            if score_diff_sum == 0.:
-                logger.warning(
-                    'No difference between force field scores. '
-                    'Exiting simplex.')
-                # Maybe excessive to repeat message, but better safe than
-                # sorry.
-                # We want to raise opt.OptError such that opt.catch_run_errors
-                # will write the best FF obtained thus far.
-                raise opt.OptError(
-                    'No difference between force field scores. '
-                    'Exiting simplex.')
+            # Calculate this value before going into loop.
+            if self.do_weighted_reflection:
+                # If zero, should break.
+                score_diff_sum = sum([x.score - self.new_ffs[-1].score
+                                      for x in self.new_ffs[:-1]])
+                if score_diff_sum == 0.:
+                    logger.warning(
+                        'No difference between force field scores. '
+                        'Exiting simplex.')
+                    # We want to raise opt.OptError such that
+                    # opt.catch_run_errors will write the best FF obtained thus
+                    # far.
+                    raise opt.OptError(
+                        'No difference between force field scores. '
+                        'Exiting simplex.')
             for i in xrange(0, len(best_ff.params)):
                 if self.do_weighted_reflection:
-                    try:
-                        inv_val = (
-                            sum([x.params[i].value *
-                                 (x.score - self.new_ffs[-1].score)
-                                 for x in self.new_ffs[:-1]])
-                            / 
-                            sum([x.score - self.new_ffs[-1].score
-                                 for x in self.new_ffs[:-1]]))
-                    except ZeroDivisionError:
-                        logger.warning(
-                            'Attempted to divide by zero while calculating the '
-                            'weighted simplex inversion point. All penalty '
-                            'function scores for the trial force fields are '
-                            'numerically equivalent.')
-                        # This needs to be a raise. opt.catch_run_errors will
-                        # catch this exception, and make sure to write the best
-                        # FF obtained up until this point.
-                        # Also, since this is now raise, rather than break, it
-                        # should exit the simplex while loop. Really, it should
-                        # exit simplex entirely.
-                        raise
+                    inv_val = (
+                        sum([x.params[i].value * score_diff_sum])
+                        / score_diff_sum)
                 else:
                     inv_val = (
                         sum([x.params[i].value for x in self.new_ffs[:-1]])

commit dec6d7ed20fcc6bba27d52cd8ab8eb130c480210

Fixed calculation of inversion point with weighted reflection
A bug was introduced in commit e40261269dbc0 that lead to the inversion point
being calculated incorrectly when weighted reflection was being used.

I believe the bug is now addressed. See discussion in issue #33 for more
details.

Reference issue #33.

simplex.py
@@ -259,7 +259,9 @@ class Simplex(opt.Optimizer):
             for i in xrange(0, len(best_ff.params)):
                 if self.do_weighted_reflection:
                     inv_val = (
-                        sum([x.params[i].value * score_diff_sum])
+                        sum([x.params[i].value * 
+                             (x.score - self.new_ffs[-1].score)
+                             for x in self.new_ffs[:-1]])
                         / score_diff_sum)
                 else:
                     inv_val = (