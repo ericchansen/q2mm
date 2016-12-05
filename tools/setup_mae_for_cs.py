40d1f811d06bb23b150d46f80e1096818f4ea2e7
Eric Hansen <ericchansen@gmail.com>
2016-12-05 12:16:48 -0500

    Removed extra comments

--- tools/setup_mae_for_cs.py
+++ tools/setup_mae_for_cs.py
@@ -73,76 +73,16 @@ def add_to_mae(filename, output, comp, tors, rca4):
             else:
                 atom.property['b_cs_comp'] = 0
 
-        # Surprisingly, this doesn't work.
-        # Schrodinger can write atom properties like this, but for whatever
-        # reason, it doesn't write bonds like this.
-        # Oddly, if the data exists in the .mae, it will, however, read the
-        # property correctly. It's a one way street. Why? Who knows.
         for bond in structure.bond:
             if set((bond.atom1.index, bond.atom2.index)) in tors_set:
                 bond.property['b_cs_tors'] = 1
             else:
                 bond.property['b_cs_tors'] = 0
 
-        # Is it something to do with writing a list at the wrong time?
-        # Well, this doesn't work.
-        # new_bonds = deepcopy(structure.bond)
-        # for bond in new_bonds:
-        #     if set((bond.atom1.index, bond.atom2.index)) in tors_set:
-        #         bond.property['b_cs_tors'] = 1
-        #     else:
-        #         bond.property['b_cs_tors'] = 0
-        # structure.bond = new_bonds
-
         structure_writer.append(structure)
 
     structure_writer.close()
     structure_reader.close()
-
-    # It seriously upsets me that I have to do this. The method above should
-    # work.
-    
-    # This cheap work around only works when only i_m_from, i_m_to and i_m_order
-    # are defined in the .mae. If you want a more complete solution, see how
-    # .mae files were read in the oldest version of filetypes.py on my GitHub.
-    # new_lines = []
-    # with open('TEMP.mae', 'r') as f:
-    #     bond_section = False
-    #     bond_colon_pos = None
-    #     for i, line in enumerate(f):
-    #         # The ordering of this crap below matters (again, I'm frustrated
-    #         # that Schrodinger's StructureWriter doesn't write the
-    #         # bond.property dictionary like it does for atom.property.
-
-    #         # This will stop the next section from execting.
-    #         # This means we're done.
-    #         if bond_colon_pos and ':::' in line:
-    #             bond_section = False
-
-    #         # This will only trigger when we're past the 1st ::: inside the bond
-    #         # section.
-    #         if bond_section and bond_colon_pos:
-    #             cols = line.split()
-    #             # Let's be explicit for your sake.
-    #             col_index, col_atom1, col_atom2, col_order = map(int, cols)
-    #             if set((col_atom1, col_atom2)) in tors_set:
-    #                 cols.append(1)
-    #             else:
-    #                 cols.append(0)
-    #             # Now put it back into the right format.
-    #             line = '  {}\n'.format(' '.join(map(str, cols)))
-
-    #         # Adding labels/keys for the bond.property dictionary.
-    #         if 'm_bond' in line:
-    #             bond_section = True
-    #         if bond_section and ':::' in line and not bond_colon_pos:
-    #             bond_colon_pos = i
-
-    #         new_lines.append(line)
-    # os.remove('TEMP.mae')
-    # new_lines.insert(bond_colon_pos, '  b_cs_tors\n')
-    # with open(output, 'w') as f:
-    #     f.writelines(new_lines)
 
 def return_parser():
     parser = argparse.ArgumentParser(
