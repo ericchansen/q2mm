dba816eff903c30a47f0efd48dfd515ffb1f2ed9
Eric Hansen <ericchansen@gmail.com>
2016-12-05 12:07:08 -0500

    Found the problem with bond properties not writing
    It's a really simple mistake. I'm a tad embarrassed. Anyway, I was appending
    the output structures at the wrong time.

--- tools/setup_mae_for_cs.py
+++ tools/setup_mae_for_cs.py
@@ -29,6 +29,7 @@ def read_com(filename):
     """
     comp = [] # Holds the COMP atoms. This is a flat list.
     tors = [] # Holds TORS. List of tuples.
+    rca4 = []
     with open(filename, 'r') as f:
         for line in f:
             cols = line.split()
@@ -36,13 +37,15 @@ def read_com(filename):
                 comp.extend(map(int, cols[1:5]))
             if cols[0] == 'TORS':
                 tors.append(tuple(map(int, cols[1:3])))
+            if cols[0] == 'RCA4':
+                rca4.append(tuple(map(int, cols[1:5])))
     # Remove all extra 0's from comp.
     # Up to 4 COMP atoms are given per line. If 3 or less are given on a line,
     # the remaining columns are 0's. Delete those meaningless 0's.
     comp = filter(lambda x: x != 0, comp)
-    return comp, tors
+    return comp, tors, rca4
 
-def add_to_mae(filename, output, comp, tors):
+def add_to_mae(filename, output, comp, tors, rca4):
     """
     Adds properties to atoms and bonds in a .mae file. The properties it adds
     were extracted from 
@@ -69,18 +72,17 @@ def add_to_mae(filename, output, comp, tors):
                 atom.property['b_cs_comp'] = 1
             else:
                 atom.property['b_cs_comp'] = 0
-        structure_writer.append(structure)
 
         # Surprisingly, this doesn't work.
         # Schrodinger can write atom properties like this, but for whatever
         # reason, it doesn't write bonds like this.
         # Oddly, if the data exists in the .mae, it will, however, read the
         # property correctly. It's a one way street. Why? Who knows.
-        # for bond in structure.bond:
-        #     if set((bond.atom1.index, bond.atom2.index)) in tors_set:
-        #         bond.property['b_cs_tors'] = 1
-        #     else:
-        #         bond.property['b_cs_tors'] = 0
+        for bond in structure.bond:
+            if set((bond.atom1.index, bond.atom2.index)) in tors_set:
+                bond.property['b_cs_tors'] = 1
+            else:
+                bond.property['b_cs_tors'] = 0
 
         # Is it something to do with writing a list at the wrong time?
         # Well, this doesn't work.
@@ -92,6 +94,8 @@ def add_to_mae(filename, output, comp, tors):
         #         bond.property['b_cs_tors'] = 0
         # structure.bond = new_bonds
 
+        structure_writer.append(structure)
+
     structure_writer.close()
     structure_reader.close()
 
@@ -101,45 +105,44 @@ def add_to_mae(filename, output, comp, tors):
     # This cheap work around only works when only i_m_from, i_m_to and i_m_order
     # are defined in the .mae. If you want a more complete solution, see how
     # .mae files were read in the oldest version of filetypes.py on my GitHub.
-    new_lines = []
-    with open('TEMP.mae', 'r') as f:
-        bond_section = False
-        bond_colon_pos = None
-        for i, line in enumerate(f):
-            # The ordering of this crap below matters (again, I'm frustrated
-            # that Schrodinger's StructureWriter doesn't write the
-            # bond.property dictionary like it does for atom.property.
+    # new_lines = []
+    # with open('TEMP.mae', 'r') as f:
+    #     bond_section = False
+    #     bond_colon_pos = None
+    #     for i, line in enumerate(f):
+    #         # The ordering of this crap below matters (again, I'm frustrated
+    #         # that Schrodinger's StructureWriter doesn't write the
+    #         # bond.property dictionary like it does for atom.property.
 
-            # This will stop the next section from execting.
-            # This means we're done.
-            if bond_colon_pos and ':::' in line:
-                bond_section = False
+    #         # This will stop the next section from execting.
+    #         # This means we're done.
+    #         if bond_colon_pos and ':::' in line:
+    #             bond_section = False
 
-            # This will only trigger when we're past the 1st ::: inside the bond
-            # section.
-            if bond_section and bond_colon_pos:
-                cols = line.split()
-                # Let's be explicit for your sake.
-                col_index, col_atom1, col_atom2, col_order = map(int, cols)
-                if set((col_atom1, col_atom2)) in tors_set:
-                    cols.append(1)
-                else:
-                    cols.append(0)
-                # Now put it back into the right format.
-                line = '  {}\n'.format(' '.join(map(str, cols)))
+    #         # This will only trigger when we're past the 1st ::: inside the bond
+    #         # section.
+    #         if bond_section and bond_colon_pos:
+    #             cols = line.split()
+    #             # Let's be explicit for your sake.
+    #             col_index, col_atom1, col_atom2, col_order = map(int, cols)
+    #             if set((col_atom1, col_atom2)) in tors_set:
+    #                 cols.append(1)
+    #             else:
+    #                 cols.append(0)
+    #             # Now put it back into the right format.
+    #             line = '  {}\n'.format(' '.join(map(str, cols)))
 
-            # Adding labels/keys for the bond.property dictionary.
-            if 'm_bond' in line:
-                bond_section = True
-            if bond_section and ':::' in line and not bond_colon_pos:
-                bond_colon_pos = i
+    #         # Adding labels/keys for the bond.property dictionary.
+    #         if 'm_bond' in line:
+    #             bond_section = True
+    #         if bond_section and ':::' in line and not bond_colon_pos:
+    #             bond_colon_pos = i
 
-            new_lines.append(line)
-    os.remove('TEMP.mae')
-
-    new_lines.insert(bond_colon_pos, '  b_cs_tors\n')
-    with open(output, 'w') as f:
-        f.writelines(new_lines)
+    #         new_lines.append(line)
+    # os.remove('TEMP.mae')
+    # new_lines.insert(bond_colon_pos, '  b_cs_tors\n')
+    # with open(output, 'w') as f:
+    #     f.writelines(new_lines)
 
 def return_parser():
     parser = argparse.ArgumentParser(
@@ -162,5 +165,5 @@ if __name__ == '__main__':
     opts = parser.parse_args(sys.argv[1:])
     if opts.out is None:
         opts.out = opts.mae
-    comp, tors = read_com(opts.com)
-    add_to_mae(opts.mae, opts.out, comp, tors)
+    comp, tors, rca4 = read_com(opts.com)
+    add_to_mae(opts.mae, opts.out, comp, tors, rca4)
