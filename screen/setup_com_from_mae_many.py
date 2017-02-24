669ab57c8d9834c6b2079f58d07aacd6c66376b5
Eric Hansen <ericchansen@gmail.com>
2017-02-24 16:02:10 -0500

    Added more job type options

--- screen/setup_com_from_mae_many.py
+++ screen/setup_com_from_mae_many.py
@@ -2,6 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 See __doc__ for setup_com_from_mae.
+
+Job Types
+---------
+cs -   Conformational search
+       Appends "_cs".
+mini - Minimization
+       Appends  "_mini".
+re   - Redundant conformer elimination
+       Appends "_re".
 """
 import argparse
 import os
@@ -22,7 +31,13 @@ def return_parser():
         help="Name of the *.mae file(s) that you'd like to generate *.com "
         "conformational search files for. Must contain the properties "
         "described in the __doc__/description for proper functioning. The "
-        "resulting *.com and *.mae file will append \"_cs_\" to the name.")
+        "resulting *.com and *.mae file a certain string to the name depending "
+        " on the MacroModel job type.")
+    parser.add_argument(
+        '-j', '--jobtype', type=str, default='cs',
+        choices=['cs', 'mini', 're'],
+        help='Job type. Choices include "cs", "mini" and "re". Default '
+        'is "cs".')
     return parser
 
 def main(opts):
@@ -31,16 +46,25 @@ def main(opts):
     """
     for file_input in opts.input:
         com_setup = MyComUtil()
-
         name, ext = os.path.splitext(file_input)
-        file_com = name + '_cs.com'
-        file_mae = name + '_cs.mae'
-
-        com_setup.my_mcmm(
-            mae_file=file_input,
-            com_file=file_com,
-            out_file=file_mae
-            )
+        if opts.jobtype == 'cs':
+            com_setup.my_mcmm(
+                mae_file=file_input,
+                com_file=name + '_cs.com',
+                out_file=name + '_cs.mae'
+                )
+        elif opts.jobtype == 'mini':
+            com_setup.my_mcmm(
+                mae_file=file_input,
+                com_file=name + '_mini.com',
+                out_file=name + '_mini.mae'
+                )
+        elif opts.jobtype == 're':
+            com_setup.my_mcmm(
+                mae_file=file_input,
+                com_file=name + '_re.com',
+                out_file=name + '_re.mae'
+                )
 
 if __name__ == '__main__':
     parser = return_parser()
