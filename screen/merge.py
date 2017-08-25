bbe2f58028e085ae34d1c0f324e6abc059ca622f
arosale4 <arosale4@users.noreply.github.com>
2017-08-25 15:05:50 -0400

    Fixed bugs from previous commit (#39)
    * Removed print lines
    
    Forgot to remove excess print statements
    
    * Fixed bugs from previous commits
    
    I had to add in another structure property to differentiate between
    aromatics we want, and those that we don't want. This propert is
    called "b_cs_accept_aromatic". Additionally, there was a problem
    where a match would be duplicated because two of the atoms in the
    match belonged to two rings, or something along this line. So now
    there is logic to determine if the match needs to be appended or
    not.

--- screen/merge.py
+++ screen/merge.py
@@ -251,7 +251,7 @@ def merge(struct_1, struct_2):
             tup = tuple(new_match_2)
             if tup not in seen:
                 seen.add(tup)
-                if struct_2.property['b_cs_first_match_only']:
+                if struct_2.property.get('b_cs_first_match_only', False):
                     seen.add(tup[::-1])
                 print(' - Unique! Continuing.')
                 # Just to look good.
@@ -352,40 +352,41 @@ def get_overlapping_atoms_in_both(struct_1, struct_2):
     print('>>> new_match_struct_1: {}'.format(new_match_struct_1))
     print('>>> new_match_struct_2: {}'.format(new_match_struct_2))
 
-
     # Sometimes a match is made that isn't what is wanted by the user and 
     # incorporates an aromatic where it should not be. This prevents aryl
     # aromatic rings that have 2 or more atoms in a match to be used as a 
     # match. -TR
     new_new_match_struct_1 = []
-    for ring in struct_1.ring:
-        atoms_in_ring = ring.getAtomList()
-        print(ring, atoms_in_ring)
-        for match in new_match_struct_1:
+    for match in new_match_struct_1:
+        use_match = True
+        for ring in struct_1.ring:
+            atoms_in_ring = ring.getAtomList()
             matched_atoms_in_ring = 0
             for atom in match:
                 if atom in atoms_in_ring:
-                    matched_atoms_in_ring += 1 
-                print(atom,matched_atoms_in_ring)
+                    matched_atoms_in_ring += 1
             if matched_atoms_in_ring >= 2:
-                print(ring.isAromatic())
-                if not ring.isAromatic():
-                    new_new_match_struct_1.append(match)
-            else:
-                new_new_match_struct_1.append(match)
+                if ring.isAromatic():
+                    if not struct_1.property.get('b_cs_accept_aromatic', False):
+                        use_match = False
+        if use_match:
+            new_new_match_struct_1.append(match)
+
     new_new_match_struct_2 = []
-    for ring in struct_2.ring:
-        atoms_in_ring = ring.getAtomList()
-        for match in new_match_struct_2:
+    for match in new_match_struct_2:
+        use_match = True
+        for ring in struct_2.ring:
+            atoms_in_ring = ring.getAtomList()
             matched_atoms_in_ring = 0
             for atom in match:
                 if atom in atoms_in_ring:
-                    matched_atoms_in_ring += 1 
+                    matched_atoms_in_ring += 1
             if matched_atoms_in_ring >= 2:
-                if not ring.isAromatic():
-                    new_new_match_struct_2.append(match)
-            else:
-                new_new_match_struct_2.append(match)
+                if ring.isAromatic():
+                    if not struct_2.property.get('b_cs_accept_aromatic', False):
+                        use_match = False
+        if use_match:
+            new_new_match_struct_2.append(match)
     print('>>> new_new_match_struct_1: {}'.format(new_new_match_struct_1))
     print('>>> new_new_match_struct_2: {}'.format(new_new_match_struct_2))
 
