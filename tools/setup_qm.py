d22f2afafb9a22734e4cf7d720708e94e6b7615e
Tony Rosales <arosale4@nd.edu>
2017-09-08 17:18:43 -0400

    Added logic for Frozen atoms and the calculation type.
    Frozen atoms are based off of internal coordinates and are a single
    string with ';' seperating different patterns. The patterns are then
    used with the schrodinger structure utility that we have used in the
    past. I opted to remove the dummy atom, if present before this step
    so that the atom numbering is correct in the gaussian *.com file.
    I'm not sure if this will be stable for all cases we are interested in.
    
    Additionally I have set up the logic to write the route section of the
    *.com file. In the case of a calcualtion with frozen coordinates a
    seperate link1 section will follow the first calculation that does
    not freeze any coordinates. I also want to include a option to
    have gaussian write a *.wfn file. This file is the wavefunction and
    can be used with the NCIPlot program to give some pretty interaction
    figures.

--- tools/setup_qm.py
+++ tools/setup_qm.py
@@ -25,13 +25,18 @@ def get_atoms_from_schrodinger_struct(structures_list):
     structures = {}
     for struct in structures_list:
         atom_list = []
+        dummy = None
         for atom in struct.atom:
+            if atom.atomic_number < 0:
+                dummy = atom
             atom_list.append((atom.element, atom.x, atom.y, atom.z))
+        if dummy:
+            struct.deleteAtoms((dummy.index),renumber_map=False)
         if 'r_mmod_Potential_Energy-MM3*' in struct.property:
             energy = struct.property['r_mmod_Potential_Energy-MM3*']
             structures[struct] = [struct.formal_charge, atom_list, energy]
         else:
-            structures[struct] = [struct.formal_charge, atom_list]
+            structures[struct] = [struct.formal_charge, atom_list]        
     structures_list = structures
     return structures_list
 
@@ -40,7 +45,7 @@ def get_atoms_from_schrodinger_struct(structures_list):
 class GaussCom():
     def __init__(self, filename, atom_list, calculation_type,
         frozen_atoms=None, memory=8, procs=8, chk=None, frequency='',
-        method='m06', basis='6-31+g*', ECP=False):
+        method='m06', basis='6-31+g*', ECP=False, opt='', mae_struct=None):
         self.filename = filename
         self.atom_list = atom_list
         self.calculation_type = calculation_type
@@ -52,6 +57,8 @@ class GaussCom():
         self.method = method
         self.basis = basis
         self.ECP = ECP
+        self.opt = opt
+        self.mae_struct = mae_struct
 
     def write_com(self):
         with open(self.filename + '.com', 'w') as f:
@@ -66,13 +73,15 @@ class GaussCom():
             #f.write(str(textwrap.wrap(' '.join(['#',
             # I want to be able to wrap text, but I can't quite figure how to
             # write the wrapped text to a file
-            f.write(' '.join(['#',
-                'empiricaldispersion=gd3 int=ultrafine',
-                self.basis,
-                self.method,
-                self.frequency,
-                '\n\n'
-                ]))
+            self.determine_method()
+            route_section = ['#','empiricaldisperion=gd3','int=ultrafine',
+                                 self.basis,self.method]
+            if self.frequency:
+                route_section.append(self.frequency)
+            if self.opt:
+                route_section.append(self.opt)
+            route_section.append('\n\n')
+            f.write(' '.join(route_section))
             f.write('SOME SORT OF STRING\n\n')
             for i,atom in enumerate(self.atom_list):
                 f.write('   '.join((' {:2}'.format(atom[0]),
@@ -83,25 +92,75 @@ class GaussCom():
                                     )))
             f.write('\n')
             if self.frozen_atoms:
-                print("do something here I don't know what yet")
+                for i,line in enumerate(self.frozen_atoms):
+                    if i+1 == len(self.frozen_atoms):
+                        f.write(line + '\n\n')
+                    else:
+                        f.write(line + '\n')
             if self.ECP:
                 print("do something here I don't know what yet")
+            if self.frozen_atoms:
+                f.write('--link1-- \n')
+                if self.chk:
+                    f.write('%chk={}\n'.format(self.chk))
+                f.write('%mem={}GB\n'.format(self.memory))
+                f.write('%nprocshared={}\n'.format(self.procs))
+                route_section = ['#','geom=allcheck','empiricaldisperion=gd',
+                                 'int=ultrafine','chkbasis']
+                if self.frequency:
+                    route_section.append(self.frequency)
+                if self.opt:
+                    route_section.append(self.opt[:-1] + ',nofreeze)')
+                route_section.append('\n\n\n')
+                f.write(' '.join(route_section))
        
-         
+    def determine_method(self):
+        if self.frozen_atoms:
+            self.opt += ' geom=modredundant'
+        if 'TS' in self.calculation_type: 
+            if not self.frequency:
+                self.frequency = 'freq=noraman'
+            self.opt = 'opt=(calcfc,ts,noeigentest,maxcycle=50)'
+        if 'GS' in self.calculation_type: 
+            self.opt = 'opt=(calcfc,maxcycle=50)'
 
-
-
-
+    def get_dictionary_of_frozen_coords(self,patterns):
+        patterns_and_type = {}
+        seperated_patterns = patterns.split(';')
+        for pattern in seperated_patterns:
+            key_value = pattern.split(',')
+            patterns_and_type[key_value[0]] = key_value[1]
+        return patterns_and_type
+            
+    def get_frozen_coords(self, dict_of_patterns, from_command_line=False):
+        frozen_coord_lines = []
+        if from_command_line:
+            dict_of_patterns = self.get_dictionary_of_frozen_coords(
+                                                              dict_of_patterns)
+        for pattern in dict_of_patterns:
+            atom_indicies = analyze.evaluate_substructure(self.mae_struct,
+                                                    pattern,
+                                                    first_match_only=False)[0]
+            str_indicies = []
+            for index in atom_indicies:
+                str_indicies.append(str(index))
+                
+            frozen_coord_lines.append('{} {} F'.format(
+                                        dict_of_patterns[pattern],
+                                        ' '.join(str_indicies)))
+        return frozen_coord_lines
 
 
 
 def main(args):
     parser = return_parser()
     opts = parser.parse_args(args)
+    # I think it is better to have the single point be default if no option
+    # is included. So the following code Should not be needed.
     #Do I need try?
-    if opts.calculationtype not in ['TS','GS','FZTS','FZGS']:
-        raise Exception('Indicate a correction calculation type from: \
-                        TS, GS, FZTS, FZGS.')
+    #if opts.calculationtype not in ['SP','TS','GS','FZTS','FZGS']:
+     #   raise Exception('Indicate a correction calculation type from: \
+     #                   TS, GS, FZTS, FZGS.')
     for filename in opts.filename:
         structures = get_sch_structs(filename,first_struct_only=opts.allstructs)
         structures = get_atoms_from_schrodinger_struct(structures)
@@ -109,7 +168,8 @@ def main(args):
             for structure in structures:
                 gaussian_file = GaussCom(filename=os.path.splitext(filename)[0],
                                     atom_list=structures[structure][1],
-                                    calculation_type=opts.calculationtype)
+                                    calculation_type=opts.calculationtype,
+                                    mae_struct=structure)
                 # Determine the basis set requested and if and ECP is needed.
                 if opts.basisset:
                     if '/' in opts.basisset:
@@ -118,11 +178,16 @@ def main(args):
                     else:
                         gaussian_file.basis = opts.basisset
                 if opts.methodfunction:
-                        gaussian_file.method = opts.methodfunction
+                    gaussian_file.method = opts.methodfunction
                 if opts.frequency:
-                        gaussian_file.frequency = 'freq=noraman'
+                    gaussian_file.frequency = 'freq=noraman'
                 if not opts.checkpoint:
-                        gaussian_file.chk = gaussian_file.filename + '.chk'
+                    gaussian_file.chk = gaussian_file.filename + '.chk'
+                if opts.frozenatoms:
+                    gaussian_file.frozen_atoms=gaussian_file.get_frozen_coords(
+                                                        opts.frozenatoms,
+                                                        from_command_line=True)
+
                 gaussian_file.write_com()                        
                                 
 
@@ -140,10 +205,11 @@ def return_parser():
             if multiple structures are being converted then the *.com files \
             will be named <parentfilename>_XXX.com where XXX is a number.')
     parser.add_argument('-ct', '--calculationtype', type=str, help='Must be \
-            one of these arguments: TS, GS, FZTS, or FZGS. TS and FZTS will \
+            one of these arguments: SP, TS, GS, FZTS, or FZGS. TS and FZTS will\
             optimize to a transition state where FZ indicates frozen coords. \
             GS and FZGS will optimize to a ground state. In the case of a TS \
-            optimization, frequency calculation will be included by default.')
+            optimization, frequency calculation will be included by default. SP\
+            will just perform a single point energy calculation.')
     parser.add_argument('-bs', '--basisset', type=str, help='Include a basis \
             set. If and ECP is needed seperate basis sets with "/". Example. \
             " -bs 6-31+g*/LANL2DZ". The ECP basis should always be last. The \
@@ -159,6 +225,11 @@ def return_parser():
     parser.add_argument('-all', '--allstructs', action='store_false',
             help='Include this argument if you just want the minimum structure\
             to optimize at the QM level.')
+    parser.add_argument('-fa', '--frozenatoms', type=str, help='Include the \
+            the atoms you wish to freeze using the Schrodinger substructure \
+            language. Also include the type of coordinate (b, a, t). Use ";" \
+            to seperate multiple coordinates. \
+            Example: -fa " C2.C2,b;PD-C2.C2,a"')
     return parser
 
 if __name__ == '__main__':
