3cf939e8c3a0d517e135f15557d398ac62b9b609
arosale4 <arosale4@users.noreply.github.com>
2018-11-30 12:29:50 -0500

    Update gradient.py

--- q2mm/gradient.py
+++ q2mm/gradient.py
@@ -83,9 +83,9 @@ class Gradient(opt.Optimizer):
 
         # Whether or not to generate parameters with these methods.
         self.do_lstsq = False
-        self.do_lagrange = False
+        self.do_lagrange = True
         self.do_levenberg = False
-        self.do_newton = False
+        self.do_newton = True
         self.do_svd = False
 
         # Particular settings for each method.
