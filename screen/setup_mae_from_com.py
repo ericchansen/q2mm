d52fe12e481792c0bfe74ea71a60532a03d532b8
jesswahlers <jwahlers@nd.edu>
2019-10-04 09:03:43 -0400

    Update screen/setup_mae_from_com.py
    Co-Authored-By: John E. Herr <17952097+jeherr@users.noreply.github.com>

--- screen/setup_mae_from_com.py
+++ screen/setup_mae_from_com.py
@@ -69,7 +69,7 @@ def read_com(filename):
     # Remove all extra 0's from comp and chig.
     # Up to 4 COMP atoms are given per line. If 3 or less are given on a line,
     # the remaining columns are 0's. Delete those meaningless 0's.
-    comp = list(filter(lambda x: x != 0, comp))
+    comp = [x for x in comp if x != 0]
     chig = list(filter(lambda x: x != 0, chig))
     return comp, tors, rca4, chig, torc
 
